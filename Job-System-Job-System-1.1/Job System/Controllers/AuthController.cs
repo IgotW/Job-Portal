9ada125465a351b1cfec1352a87a275ecddf3e8a
SawDirk <wentigot.1@gmail.com>
2024-12-09 23:16:30 +0800

    Success Connected to Database
    candidate and company can already use the signup that will put on database

--- Job-System-Job-System-1.1/Job System/Controllers/AuthController.cs
+++ Job-System-Job-System-1.1/Job System/Controllers/AuthController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using JobPortal.Models;
+using Job_System.Data;
+using Job_System.Models;
 
 namespace JobPortal.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AuthController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Login()
         {
             return View();
@@ -19,10 +27,34 @@ namespace JobPortal.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CandidateRegister(Candidate obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Candidates.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(obj);
+        }
 
         public IActionResult CompanyRegister()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CompanyRegister(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Companies.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(obj);
         }
     }
 }
